f706bfe286df48ef8c00542d661a2c10a77c0d50
jakopo87 <jak1@hotmail.it>
2019-08-20 14:21:09 +0200

    better comments

--- src/extension.ts
+++ src/extension.ts
@@ -5,14 +5,20 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    /* Default value by package.json - CORRECT */
     console.log(vscode.workspace.getConfiguration('helloworld').inspect('test_package'));
-    console.log(vscode.workspace.getConfiguration('helloworld').inspect('test_workspace'));
-    console.log(vscode.workspace.getConfiguration('helloworld_nested').inspect('test_package'));
-    console.log(vscode.workspace.getConfiguration('helloworld_nested').inspect('test_workspace'));
-
     console.log(vscode.workspace.getConfiguration('helloworld').get('test_package'));
+
+    /* Overwritten value by workspace - CORRECT */
+    console.log(vscode.workspace.getConfiguration('helloworld').inspect('test_workspace'));
     console.log(vscode.workspace.getConfiguration('helloworld').get('test_workspace'));
+
+    /* Default value by package.json - NOT WORKING*/
+    console.log(vscode.workspace.getConfiguration('helloworld_nested').inspect('test_package'));
     console.log(vscode.workspace.getConfiguration('helloworld_nested').get('test_package'));
+
+    /* Overwritten value by workspace - CORRECT */
+    console.log(vscode.workspace.getConfiguration('helloworld_nested').inspect('test_workspace'));
     console.log(vscode.workspace.getConfiguration('helloworld_nested').get('test_workspace'));
 }
 
